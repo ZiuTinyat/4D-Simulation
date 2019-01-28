d6df73015572859b375a635fa285d15664d21fdc
Tianyi Zhao <tz1@andrew.cmu.edu>
2019-01-28 16:37:15 -0500

    init project

---
+++ 4D-Simulation/Assets/Scripts/Rotation4D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HyperSpace {
+    public struct Rotation4D : IEquatable<Rotation4D> {
+
+        public float xy, xz, xw, yz, yw, zw;
+
+        public bool Equals(Rotation4D other) {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ 4D-Simulation/Assets/Scripts/Transform4D.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HyperSpace {
+    public class Transform4D : MonoBehaviour {
+
+        public Vector4 position;
+        public Vector4 localPosition;
+        public Rotation4D rotation;
+        public Rotation4D localRotaion;
+
+        
+    }
+}
